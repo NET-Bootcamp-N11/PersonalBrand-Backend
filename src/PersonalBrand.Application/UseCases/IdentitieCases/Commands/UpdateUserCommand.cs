c95ad20c550e7ed11a479fa72ede8665ab9fb886
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-05-15 12:18:48 +0500

    bug fix

--- src/PersonalBrand.Application/UseCases/IdentitieCases/Commands/UpdateUserCommand.cs
+++ src/PersonalBrand.Application/UseCases/IdentitieCases/Commands/UpdateUserCommand.cs
@@ -17,6 +17,7 @@ namespace PersonalBrand.Application.UseCases.IdentitieCases.Commands
         public string Email { get; set; }
         public string UserName { get; set; }
         public string PhoneNumber { get; set; }
+        //
 
     }
 }
