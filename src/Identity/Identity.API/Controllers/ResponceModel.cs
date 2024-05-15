4bfcaef32d314fb65fe2f7a99557cf6103830f5d
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-05-15 12:03:01 +0500

    Add auth controller

---
+++ src/Identity/Identity.API/Controllers/ResponceModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Identity.API.Controllers
+{
+    internal class ResponceModel : ModelStateDictionary
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
